50e4e52ebdf26b370b8abedb89c19962515f858a
Jamie McCann <jamie.mccann@umu.se>
2024-11-12 11:21:32 +0100

    Removing old ddl files

--- plantgenie_heatmaps/sqlite_ddls/genome_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS genomes (
-    id INTEGER PRIMARY KEY,
-    species_id INTEGER,
-    version TEXT,
-    description TEXT,
-    doi TEXT,
-    publication_date INTEGER,
-    FOREIGN KEY(species_id) REFERENCES species(id)
-);
