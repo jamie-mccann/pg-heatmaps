049885447004cf300374b6345bc5cfab6577b870
Jamie McCann <jamie.mccann@umu.se>
2024-12-02 15:23:09 +0100

    Fixing vite config noe

--- frontends/exheatmap/vite.config.ts
+++ frontends/exheatmap/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/" : "/static/", //: "/",
+  base: mode === "production" ? "/static/" : "/",
   plugins: [react()],
   server: {
     proxy: {
